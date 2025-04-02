f6c875efe79895ebf79a3b7d50787a9cab24173e
Sergio Romano <sergioromano@Sergios-MacBook-Air.local>
2025-04-02 18:03:28 -0300

    fix: remove unused logs

--- src/get-posts.ts
+++ src/get-posts.ts
@@ -4,9 +4,7 @@ import { Post } from "./types";
 export const getPosts = async () => {
   try {
     const response = await fetch(`${BASE_URL}/posts`);
-    console.log({ response });
     const json = await response.json();
-    console.log({ json });
     return json as Post[];
   } catch (error) {
     console.error(error);
